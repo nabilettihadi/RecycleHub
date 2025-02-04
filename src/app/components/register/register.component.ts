b37de93e841496055c1f5ebd8b25da31b2e3f2c7
Nabil Ettihadi <nabilettihadi.ma@gmail.com>
2025-02-04 19:55:39 +0100

    feat: Add register component with reactive form

---
+++ src/app/components/register/register.component.ts
@@ -0,0 +1,63 @@
+import { Component } from '@angular/core';
+import { FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { RegisterForm } from '../../models/auth.model';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [ReactiveFormsModule, RouterLink],
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent {
+  registerForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    confirmPassword: ['', Validators.required],
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    address: this.fb.group({
+      street: ['', Validators.required],
+      city: ['', Validators.required],
+      zipCode: ['', Validators.required]
+    }),
+    phoneNumber: ['', Validators.required],
+    dateOfBirth: ['', Validators.required],
+    profilePicture: ['']
+  });
+
+  errorMessage = '';
+  successMessage = '';
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService
+  ) {}
+
+  onSubmit() {
+    if (this.registerForm.invalid) return;
+    if (this.registerForm.value.password !== this.registerForm.value.confirmPassword) {
+      this.errorMessage = 'Les mots de passe ne correspondent pas';
+      return;
+    }
+
+    const formValue: RegisterForm = {
+      ...this.registerForm.value,
+      address: this.registerForm.value.address!,
+      dateOfBirth: new Date(this.registerForm.value.dateOfBirth!)
+    } as RegisterForm;
+
+    this.authService.register(formValue).subscribe({
+      next: () => {
+        this.successMessage = 'Inscription réussie ! Redirection...';
+        this.errorMessage = '';
+      },
+      error: (err) => {
+        this.errorMessage = err.message || 'Une erreur est survenue';
+        this.successMessage = '';
+      }
+    });
+  }
+}
